e1ad392bc1b976f5db994417e27b324d654b3eac
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-06-02 01:56:34 +0200

    Refactorized LoginForm

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { useAppDispatch } from "../../redux/hooks/hooks";
 import { loginUserThunk } from "../../thunks/userThunks";
-import { LoginFormData } from "../../types/UserTypes";
+import { Label, LoginFormData } from "../../types/UserTypes";
+import EyeIcon from "../Icons/EyeIcon";
+import UserIcon from "../Icons/UserIcon";
 
 const blankForm: LoginFormData = {
   username: "",
@@ -11,6 +13,21 @@ const blankForm: LoginFormData = {
 const LoginForm = (): JSX.Element => {
   const [formData, setFormData] = useState<LoginFormData>(blankForm);
   const dispatch = useAppDispatch();
+
+  const labels: Label[] = [
+    {
+      id: "username",
+      type: "text",
+      text: "Enter username",
+      icon: <UserIcon />,
+    },
+    {
+      id: "password",
+      type: "password",
+      text: "Enter password",
+      icon: <EyeIcon />,
+    },
+  ];
 
   const changeFormData = (event: {
     target: { id: string; value: string };
@@ -25,25 +42,58 @@ const LoginForm = (): JSX.Element => {
   };
 
   return (
-    <>
-      <form onSubmit={submitFormData} noValidate autoComplete="off">
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          placeholder="Username..."
-          onChange={changeFormData}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="text"
-          id="password"
-          placeholder="Password..."
-          onChange={changeFormData}
-        />
-        <input type="submit" value="LOGIN" onSubmit={submitFormData} />
-      </form>
-    </>
+    <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+      <div className="max-w-lg mx-auto">
+        <form
+          action="register"
+          onSubmit={submitFormData}
+          className="p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl"
+        >
+          <p className="text-2xl font-bold text-left text-indigo-600 sm:text-3xl">
+            Log in
+          </p>
+          {labels.map((label) => (
+            <div>
+              <label
+                htmlFor={label.id}
+                hidden={true}
+                className="text-sm font-medium"
+              >
+                {label.text}
+              </label>
+
+              <div className="relative mt-1">
+                <input
+                  type={label.type}
+                  id={label.id}
+                  className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                  placeholder={label.text}
+                  onChange={changeFormData}
+                />
+
+                <span className="absolute inset-y-0 inline-flex items-center right-4">
+                  {label.icon}
+                </span>
+              </div>
+            </div>
+          ))}
+          <button
+            type="submit"
+            onSubmit={submitFormData}
+            className="block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg"
+          >
+            Sign in
+          </button>
+
+          <p className="text-sm text-center text-gray-500">
+            No account?
+            <a className="underline" href="/login">
+              Sign up
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
   );
 };
 

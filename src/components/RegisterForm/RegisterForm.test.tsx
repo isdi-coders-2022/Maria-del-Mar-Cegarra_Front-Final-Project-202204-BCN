8a2d8157345c5d040372b3dab9c8fc74d2abc495
Maria del Mar <maria.cegarra2303@gmail.com>
2022-06-01 11:42:19 +0200

    Tested onChange function

--- src/components/RegisterForm/RegisterForm.test.tsx
+++ src/components/RegisterForm/RegisterForm.test.tsx
@@ -1,17 +1,17 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import RegisterForm from "./RegisterForm";
 
 describe("Given the RegisterForm component", () => {
+  const expectedLabels: string[] = [
+    "Name",
+    "Username",
+    "Email",
+    "Password",
+    "Repeat password",
+  ];
   describe("When its invoked", () => {
     test("Then it should render 5 labels with accessible names: name, username, email, password, repeat password and a REGISTER submit", () => {
-      const expectedLabels: string[] = [
-        "Name",
-        "Username",
-        "Email",
-        "Password",
-        "Repeat password",
-      ];
-
       render(<RegisterForm />);
       const allInputs = expectedLabels.map((expectedLabel) =>
         screen.getByLabelText(expectedLabel, { selector: "input" })
@@ -29,6 +29,22 @@ describe("Given the RegisterForm component", () => {
       const submit = screen.getByRole("button", { name: expectedSubmit });
 
       expect(submit).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user types 'asdfg' on the inputs", () => {
+    test("Then the value of the inpust shoould be 'asdfg'", () => {
+      const expectedInputValue = "asdfg";
+
+      render(<RegisterForm></RegisterForm>);
+      const allInputs = expectedLabels.map((expectedLabel) =>
+        screen.getByLabelText(expectedLabel, { selector: "input" })
+      );
+      allInputs.forEach((input) => userEvent.type(input, expectedInputValue));
+
+      allInputs.forEach((input) =>
+        expect(input).toHaveValue(expectedInputValue)
+      );
     });
   });
 });

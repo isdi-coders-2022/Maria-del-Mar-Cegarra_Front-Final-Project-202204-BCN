4411915fd9f49463bb99ba121b6219fa8bab9c1b
Maria del Mar <maria.cegarra2303@gmail.com>
2022-06-02 12:39:16 +0200

    Created Login and Form pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,11 @@
 import LoginForm from "../components/LoginForm/LoginForm";
 
 const LoginPage = (): JSX.Element => {
-  return <LoginForm />;
+  return (
+    <div className="bg-main-img bg-scroll bg-no-repeat bg-cover h-screen w-screen ">
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,7 +1,11 @@
 import RegisterForm from "../components/RegisterForm/RegisterForm";
 
 const RegisterPage = (): JSX.Element => {
-  return <RegisterForm />;
+  return (
+    <div className="bg-main-img bg-scroll bg-no-repeat bg-cover h-screen w-screen ">
+      <RegisterForm />
+    </div>
+  );
 };
 
 export default RegisterPage;

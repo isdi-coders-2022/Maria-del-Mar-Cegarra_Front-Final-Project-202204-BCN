64a0b5286964d531008938dc5cf03638915caaf9
Maria del Mar <maria.cegarra2303@gmail.com>
2022-06-02 15:07:27 +0200

    Formatted Navigation

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,9 @@
 import LoginForm from "../components/LoginForm/LoginForm";
-import Navigation from "../components/Navigation/Navigation";
 
 const LoginPage = (): JSX.Element => {
   return (
     <div className="bg-main-img bg-scroll bg-no-repeat bg-cover h-screen w-screen ">
       <LoginForm />
-      <Navigation />
     </div>
   );
 };

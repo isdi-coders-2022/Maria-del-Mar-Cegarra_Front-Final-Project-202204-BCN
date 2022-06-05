04e21267b90bdb295732d010e7e398a69b718001
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-06-05 22:31:42 +0200

    Refactorized user thunks

--- src/redux/thunks/userThunks.ts
+++ src/redux/thunks/userThunks.ts
@@ -7,34 +7,83 @@ import {
   UserState,
 } from "../../types/UserTypes";
 import { loginActionCreator } from "../features/userSlice";
+import { showAdviseThunk, showErrorThunk } from "./UIThunks";
 
 const apiUrl = process.env.REACT_APP_API_URL;
+
+export const showAdviseNoRegisterData = showAdviseThunk(
+  "User data not provided",
+  "Please fill all the fields and ty again"
+);
+
+export const showAdviseAlreadyRegistered = showAdviseThunk(
+  "User already registered",
+  "Please try again"
+);
+export const showErrorRegisterUser = showErrorThunk(
+  "An error has occurred",
+  "Please, try again later"
+);
+
+export const showAdviseWrongUsernamePaswword = showAdviseThunk(
+  "Wrong username or password",
+  "Please try again"
+);
+
+export const showAdviseLoginUser = showAdviseThunk(
+  "User data not provided",
+  "Please fill all the fields and ty again"
+);
+export const showErrorLoginUser = showErrorThunk(
+  "An error has occurred",
+  "Please, try again later"
+);
 
 export const registerUserThunk =
   (formData: RegisterFormData) => async (dispatch: AppDispatch) => {
     const { name, username, password } = formData;
-    const { data } = await axios.post(`${apiUrl}user/register`, {
-      name,
-      username,
-      password,
-    });
-    if (data) {
+    if (!(name || username || password)) {
+      dispatch(showAdviseNoRegisterData);
+    }
+    try {
+      const { data } = await axios.post(`${apiUrl}user/register`, {
+        name,
+        username,
+        password,
+      });
+      if (!data) {
+        dispatch(showAdviseAlreadyRegistered);
+        return;
+      }
       const userInfo: UserState = { ...jwtDecode(data.token), logged: true };
       localStorage.setItem("token", data.token);
+
       dispatch(loginActionCreator(userInfo));
+    } catch (error) {
+      dispatch(showErrorRegisterUser);
     }
   };
 
 export const loginUserThunk =
   (formData: LoginFormData) => async (dispatch: AppDispatch) => {
     const { username, password } = formData;
-    const { data } = await axios.post(`${apiUrl}user/login`, {
-      username,
-      password,
-    });
-    if (data) {
+    if (!(username || password)) {
+      dispatch(showAdviseLoginUser);
+    }
+    try {
+      const { data } = await axios.post(`${apiUrl}user/login`, {
+        username,
+        password,
+      });
+      if (!data) {
+        dispatch(showAdviseWrongUsernamePaswword);
+        return;
+      }
       const userInfo: UserState = { ...jwtDecode(data.token), logged: true };
       localStorage.setItem("token", data.token);
+
       dispatch(loginActionCreator(userInfo));
+    } catch (error) {
+      dispatch(showErrorLoginUser);
     }
   };

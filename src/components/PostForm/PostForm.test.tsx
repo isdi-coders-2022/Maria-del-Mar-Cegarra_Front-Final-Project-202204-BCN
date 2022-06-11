343992fbe6c5c91b81600db791051f8022bcb91b
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-06-11 17:49:04 +0200

    Tested rendering of an image when ipload

--- src/components/PostForm/PostForm.test.tsx
+++ src/components/PostForm/PostForm.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { FILE } from "dns";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "../../redux/store/store";
@@ -28,20 +29,50 @@ describe("Given the PostForm component", () => {
 
       labels.map((label) => expect(label).toBeInTheDocument());
     });
-  });
 
-  test("Then it should render a submit and call dispatch when clicked", () => {
-    const expectedSubmitText = "Add Post";
-    render(
-      <BrowserRouter>
-        <Provider store={store}>
-          <PostForm />
-        </Provider>
-      </BrowserRouter>
-    );
-    const submit = screen.getByRole("button", { name: expectedSubmitText });
-    userEvent.click(submit);
+    test("Then it should render a submit and call dispatch when clicked", () => {
+      const expectedSubmitText = "Add Post";
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <PostForm />
+          </Provider>
+        </BrowserRouter>
+      );
+      const submit = screen.getByRole("button", { name: expectedSubmitText });
+      userEvent.click(submit);
 
-    expect(mockDispatch).toHaveBeenCalled();
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+
+    describe("Then it should render an input type file", () => {
+      const expectedFileLabelText = "Picture";
+      const expectedAltImage = "Your uploads";
+      const expectedIdButton = "Delete";
+      const fileValue = [{ picture: "PictureName" }];
+      const pictureString = JSON.stringify(fileValue);
+      const pictureBlob = new Blob([pictureString]);
+      const pictureFile = new File([pictureBlob], "values.json", {
+        type: "application/JSON",
+      });
+      global.URL.createObjectURL = jest.fn();
+      test("And render an image and a delete button when clicked", async () => {
+        render(
+          <BrowserRouter>
+            <Provider store={store}>
+              <PostForm />
+            </Provider>
+          </BrowserRouter>
+        );
+
+        const fileInput = screen.getByLabelText(expectedFileLabelText);
+        userEvent.upload(fileInput, pictureFile);
+        const image = screen.getByRole("img", { name: expectedAltImage });
+        const button = screen.getByTestId(expectedIdButton);
+
+        await waitFor(() => expect(image).toBeInTheDocument());
+        await waitFor(() => expect(button).toBeInTheDocument());
+      });
+    });
   });
 });

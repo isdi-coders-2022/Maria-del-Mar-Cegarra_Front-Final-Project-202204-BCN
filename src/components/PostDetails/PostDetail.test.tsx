dcb09afb08030dd5b03df9b27b4a1cbf600043b4
Maria del Mar <maria.cegarra2303@gmail.com>
2022-06-13 19:06:34 +0200

    Tested PostDetail component

---
+++ src/components/PostDetails/PostDetail.test.tsx
@@ -0,0 +1,91 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { mockPosts } from "../../mocks/postMocks";
+import { store } from "../../redux/store/store";
+import PostDetails from "./PostDetails";
+
+const mockDispatch = jest.fn();
+const mockNavigate = jest.fn();
+
+jest.mock("../../redux/hooks/hooks", () => ({
+  ...jest.requireActual("../../redux/hooks/hooks"),
+  useAppDispatch: () => mockDispatch,
+}));
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
+describe("Given the DetailPost component", () => {
+  describe("When its invoked receiving a post object", () => {
+    test("Then it should render 3 images", () => {
+      const expectedNumberImages = 3;
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <PostDetails post={mockPosts[0]} />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const images = screen.getAllByRole("img");
+
+      expect(images).toHaveLength(expectedNumberImages);
+    });
+
+    test("Then the image of the first image should have src 'Picture 1.jpg'", () => {
+      const expectedImage =
+        "https://maria-del-mar-cegarra-back-final-project.onrender.com/uploads/Picture 1";
+      const expectedAlt = "Post";
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <PostDetails post={mockPosts[0]} />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const image = screen.getByRole("img", { name: expectedAlt });
+
+      expect(image).toHaveAttribute("src", expectedImage);
+    });
+  });
+
+  describe("When its invoked and the id of the user owner of the post matches with the id of the user logged", () => {
+    test("Then it should render a button 'Edit' and call dispatch when clicked", () => {
+      const expectedButton = "Edit";
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <PostDetails post={mockPosts[0]} />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const editButton = screen.getByRole("button", { name: expectedButton });
+      userEvent.click(editButton);
+
+      expect(mockNavigate).toHaveBeenCalled();
+    });
+
+    test("Then it should render a button 'Delete' and call dispatch when clicked", () => {
+      const expectedButton = "Delete";
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <PostDetails post={mockPosts[0]} />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const deleteButton = screen.getByRole("button", { name: expectedButton });
+      userEvent.click(deleteButton);
+
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+  });
+});

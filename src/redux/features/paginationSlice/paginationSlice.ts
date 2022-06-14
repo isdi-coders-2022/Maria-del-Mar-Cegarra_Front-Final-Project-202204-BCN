245abbabc276042c215cc0710a800ec7b4a42d9d
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-06-14 03:45:54 +0200

    Created paginationSlice with action creator for user and public posts

---
+++ src/redux/features/paginationSlice/paginationSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Pagination {
+  publicPostsPage: number;
+  userPostsPage: number;
+}
+
+const initialState: Pagination = {
+  publicPostsPage: 1,
+  userPostsPage: 1,
+};
+
+export const paginationSlice = createSlice({
+  name: "pagination",
+  initialState,
+  reducers: {
+    loadMorePublicPosts: (pagination) => ({
+      ...initialState,
+      publicPostsPage: pagination.publicPostsPage + 1,
+    }),
+    loadMoreUserPosts: (pagination) => ({
+      ...initialState,
+      userPostsPage: pagination.userPostsPage + 1,
+    }),
+    goToStartPublicPosts: (pagination) => ({
+      ...pagination,
+      publicPostsPage: 1,
+    }),
+    goToStartUserPosts: (pagination) => ({
+      ...pagination,
+      userPostsPage: 1,
+    }),
+  },
+});
+
+export default paginationSlice.reducer;
+
+export const {
+  loadMorePublicPosts: loadMorePublicPostsActionCreator,
+  loadMoreUserPosts: loadMoreUserPostsActionCreator,
+  goToStartPublicPosts: goToStartPublicPostsActionCreator,
+  goToStartUserPosts: goToStartUserPostsActionCreator,
+} = paginationSlice.actions;

56618022bb71876ee29d3fdd607f55259bec2fd6
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-06-15 03:31:22 +0200

    Added role img to icon

--- src/components/Icons/ExitIcon.tsx
+++ src/components/Icons/ExitIcon.tsx
@@ -3,6 +3,7 @@ import { Props } from "./Props";
 const ExitIcon = ({ color }: Props): JSX.Element => {
   return (
     <svg
+      role="img"
       xmlns="http://www.w3.org/2000/svg"
       className={`h-6 w-6 ${color}`}
       fill="none"

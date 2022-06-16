485e9cf31072b0e50fc537477491ef7130019c8a
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-06-16 02:39:55 +0200

    Added margin on form

--- src/pages/AddPostPage/AddPostPage.tsx
+++ src/pages/AddPostPage/AddPostPage.tsx
@@ -3,7 +3,7 @@ import PostForm from "../../components/PostForm/PostForm";
 const AddPostPage = (): JSX.Element => {
   return (
     <div className="flex justify-center transition-all">
-      <div className="backdrop-blur-md absolute md:w-8/12 lg:w-8/12 border rounded-3xl center content-center bottom-0 top-16">
+      <div className="backdrop-blur-md relative md:w-8/12 lg:w-8/12 border rounded-3xl center content-center bottom-0 top-16 pb-40">
         <h1 className="text-6xl m-4 font-semibold">Add Post</h1>
         <PostForm />
       </div>
